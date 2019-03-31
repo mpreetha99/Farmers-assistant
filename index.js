8b4e6a0747d4131b9fa5fc0fc0388040c3749a46
mpreetha99 <43870680+mpreetha99@users.noreply.github.com>
2019-03-31 11:41:13 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 let request = require('request');
-const express = require('express')
+const express = require('express');
 const bodyParser = require('body-parser');
-const app = express()
+let rice = require('./rice.json');
+let wheat = require('./wheat.json');
+let jowar = require('./jowar.json');
+const app = express();
 let apiKey = '6bda9c040fdbca6f7df39a1622a7fb9d';
-let city = 'mumbai';
 var lati;
 var long;
+var weather;
+var message;
+var w,t,h,p,r,z,ph;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
@@ -16,23 +21,45 @@ app.get('/', function (req, res) {
 
 app.post('/', function (req, res) {
   
-  console.log(req.body.crop); 
+  console.log(req.body.crop);
+  r=req.body.crop; 
   console.log(req.body.latitude)
   lati=req.body.latitude;
   long=req.body.longitude;
   console.log(req.body.longitude)
   let url = `http://api.openweathermap.org/data/2.5/weather?lat=${lati}&lon=${long}&units=metric&appid=${apiKey}`
+  
   request(url, function (err, response, body) {
 
     if(err){
       console.log('error:', err);
     } else {
-      let weather = JSON.parse(body)
-     let message = ` ${weather.name}   ${weather.main.temp}  ${weather.main.humidity} ${weather.main.pressure} !`;
+      weather = JSON.parse(body)
+      message = ` ${weather.name}   ${weather.main.temp}  ${weather.main.humidity} ${weather.main.pressure} `;
+      w=weather.name;
+      t=weather.main.temp;
+      h=weather.main.humidity;
+      p=weather.main.pressure;
+      ph=6.9;
      console.log(message);
+     if(r=='rice')
+     {
+      m = ` Location ${weather.name}    ${rice.p4.n} `;
+     res.render('index', {weather:m , error: null});
+     }
+     if(r=='wheat')
+     {
+      m = ` Location ${weather.name}    ${wheat.p1.n} `;
+     res.render('index', {weather:m , error: null});
+     }
+     if(r=='jowar')
+     {
+      m = ` Location ${weather.name}    ${jowar.p1.n} `;
+     res.render('index', {weather:m , error: null});
+     }
     }
   })
-  res.render('index', {weather: 'hi', error: null});
+  
 })
 
 app.listen(3000, function () {
