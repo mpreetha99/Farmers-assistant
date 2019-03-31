a858ad675c494ca5d8efe8f2da53ab061fa2105f
mpreetha99 <43870680+mpreetha99@users.noreply.github.com>
2019-03-31 05:45:47 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const bodyParser = require('body-parser');
 const app = express()
 let apiKey = '6bda9c040fdbca6f7df39a1622a7fb9d';
 let city = 'mumbai';
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+var lati;
+var long;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
@@ -14,21 +15,27 @@ app.get('/', function (req, res) {
 })
 
 app.post('/', function (req, res) {
-  console.log(req.body.crop);
-  res.render('index');  
+  
+  console.log(req.body.crop); 
+  console.log(req.body.latitude)
+  lati=req.body.latitude;
+  long=req.body.longitude;
+  console.log(req.body.longitude)
+  let url = `http://api.openweathermap.org/data/2.5/weather?lat=${lati}&lon=${long}&units=metric&appid=${apiKey}`
+  request(url, function (err, response, body) {
+
+    if(err){
+      console.log('error:', err);
+    } else {
+      let weather = JSON.parse(body)
+     let message = ` ${weather.name}   ${weather.main.temp}  ${weather.main.humidity} ${weather.main.pressure} !`;
+     console.log(message);
+    }
+  })
+  res.render('index', {weather: 'hi', error: null});
 })
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
-})
-
-request(url, function (err, response, body) {
-
-  if(err){
-    console.log('error:', err);
-  } else {
-    let weather = JSON.parse(body)
-    let message = ` ${weather.coord.lon}  ${weather.coord.lat}  ${weather.main.temp}  ${weather.main.humidity} ${weather.main.pressure} !`;
-   console.log(message);
-  }
 });
+
